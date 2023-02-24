d712325dee6e6439f525df742081a4b7dad35355
Samratrpal01 <Samratrpal01.com>
2023-02-25 02:19:58 +0530

    Updating Files

--- src/header.h
+++ src/header.h
@@ -1,122 +1,168 @@
-// function to check if a number is prime
-void isPrime()
-{
-    int n;
-    cout << "Enter a number to check if it's prime: ";
-    cin >> n;
-    if (n <= 1)
-    {
-        cout << n << " is not a prime number.\n";
-        return false;
+#include <bits/stdc++.h>
+using namespace std;
+
+void even() {
+  cout << "Enter Number to Check Even\n";
+  int n;
+  cin >> n;
+  if (n % 2 == 0)
+    std::cout << "Entered Number Is Even\n";
+  else
+    std::cout << "Entered Number Is Not Even\n";
+}
+
+void odd() {
+  cout << "Enter Number to check odd\n";
+  int n;
+  cin >> n;
+  if (n & 1)
+    cout << "Entered Number is Odd\n";
+  else
+    cout << "Entered Number is not Odd\n";
+}
+
+void even_odd() {
+  cout << "Enter Number to Check Even and odd\n";
+  int n;
+  cin >> n;
+  if (n & 1)
+    cout << "Entered Number is Odd\n";
+  else
+    cout << "Entered Number is even\n";
+}
+
+void palindrome() {
+  cout << "Enter Input to check Number is Palindrome or Not\n";
+  string s;
+  cin >> s;
+  string temp = s;
+  reverse(s.begin(), s.end());
+  if (temp == s)
+    cout << "Entered Number is Palindrome\n";
+  else
+    cout << "Entered Number is not Palindrome\n";
+}
+
+void reverse() {
+  cout << "Insert Input to Reverse\n";
+  string s;
+  cin >> s;
+  reverse(s.begin(), s.end());
+  cout << "Reverse of the Input is :"
+       << " " << s << endl;
+}
+bool checkPrime(int n) {
+
+  cin >> n;
+  if (n <= 1) {
+    return false;
+  }
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0) {
+      return false;
     }
-    for (int i = 2; i <= sqrt(n); i++)
-    {
-        if (n % i == 0)
-        {
-            cout << n << " is not a prime number.\n";
-        }
+  }
+  return true;
+}
+void isPrime() {
+  int n;
+  cout << "Enter a number to check if it's prime: ";
+  cin >> n;
+  if (n <= 1) {
+    cout << n << " is not a prime number.\n";
+    return false;
+  }
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0) {
+      cout << n << " is not a prime number.\n";
     }
-    cout << n << " is a prime number.\n";
+  }
+  cout << n << " is a prime number.\n";
 }
 
 // function to check if a number is a happy number
-void isHappy()
-{
-    int n;
-    cout << "Enter a number to check if it's happy: ";
-    cin >> n;
-    unordered_set<int> visited;
-    while (n != 1 && !visited.count(n))
-    {
-        visited.insert(n);
-        int sum = 0;
-        while (n > 0)
-        {
-            int digit = n % 10;
-            sum += digit * digit;
-            n /= 10;
-        }
-        n = sum;
+void isHappy() {
+  int n;
+  cout << "Enter a number to check if it's happy: ";
+  cin >> n;
+  unordered_set<int> visited;
+  while (n != 1 && !visited.count(n)) {
+    visited.insert(n);
+    int sum = 0;
+    while (n > 0) {
+      int digit = n % 10;
+      sum += digit * digit;
+      n /= 10;
     }
-    if (n == 1)
-    {
-        cout << "The entered number is a happy number.\n";
-    }
-    else
-    {
-        cout << "The entered number is not a happy number.\n";
-    }
+    n = sum;
+  }
+  if (n == 1) {
+    cout << "The entered number is a happy number.\n";
+  } else {
+    cout << "The entered number is not a happy number.\n";
+  }
 }
 
 // function to check if a number is an Armstrong number
-void isArmstrong()
-{
-    int n;
-    cout << "Enter a number to check if it's an Armstrong number: ";
-    cin >> n;
-    int sum = 0, original = n;
-    while (n > 0)
-    {
-        int digit = n % 10;
-        sum += pow(digit, 3);
-        n /= 10;
-    }
-    if (sum == original)
-    {
-        cout << "The given entered number is an Armstrong number.\n";
-    }
-    else
-    {
-        cout << "The entered number is not an Armstrong number.\n";
-    }
+void isArmstrong() {
+  int n;
+  cout << "Enter a number to check if it's an Armstrong number: ";
+  cin >> n;
+  int sum = 0, original = n;
+  while (n > 0) {
+    int digit = n % 10;
+    sum += pow(digit, 3);
+    n /= 10;
+  }
+  if (sum == original) {
+    cout << "The entered number is an Armstrong number.\n";
+  } else {
+    cout << "The entered number is not an Armstrong number.\n";
+  }
 }
 
-void nthPrime()
-{
-    int n;
-    cout << "Enter n to find nth prime: ";
-    cin >> n;
-    int count = 0, num = 2;
-    while (count < n)
-    {
-        if (isPrime(num))
-        {
-            count++;
-        }
-        num++;
+void nthPrime() {
+  int n;
+  cout << "Enter n to find nth prime: ";
+  cin >> n;
+  int count = 0, num = 2;
+  while (count < n) {
+    if (checkPrime(num)) {
+      count++;
     }
-    cout << "The " << n << "th prime is: " << nthPrime(n) << endl;
+    num++;
+  }
+  cout << "The " << n << "th prime is: " << num << endl;
 }
 
-int factorial(int n)
-{
-    if (n <= 1)
-    {
-        return 1;
-    }
-    return n * factorial(n - 1);
+int factorial(int n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
-void isStrong()
-{
-    int n;
-    cout << "Enter n to check if it's a strong number: ";
-    cin >> n;
-    int sum = 0, original = n;
-    while (n > 0)
-    {
-        int digit = n % 10;
-        sum += factorial(digit);
-        n /= 10;
-    }
+void fact() {
+  cout << "Enter Number to find Factorail" << endl;
+  int n;
+  cin >> n;
+  cout << factorial(n) << endl;
+}
 
-    if (sum == original)
-    {
-        cout << n << " is a strong number" << endl;
-    }
-    else
-    {
-        cout << n << " is not a strong number" << endl;
-    }
-    return sum == original;
+void isStrong() {
+  int n;
+  cout << "Enter n to check if it's a strong number: ";
+  cin >> n;
+  int sum = 0, original = n;
+  while (n > 0) {
+    int digit = n % 10;
+    sum += factorial(digit);
+    n /= 10;
+  }
+
+  if (sum == original) {
+    cout << n << " is a strong number" << endl;
+  } else {
+    cout << n << " is not a strong number" << endl;
+  }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,59 @@
+#include "header.h"
 #include <bits/stdc++.h>
 #include <fstream>
 #include <iostream>
 #include <string>
+
 using namespace std;
 map<string, string> mp;
+void callFun(string s) {
+  if (s == "even")
+    even();
+  else if (s == "odd")
+    odd();
+  else if (s == "even_odd")
+    even_odd();
+  else if (s == "palindrome")
+    palindrome();
+  else if (s == "prime")
+    isPrime();
+  else if (s == "Happy")
+    isHappy();
+  else if (s == "armstrong")
+    isArmstrong();
+  else if (s == "nthprime")
+    nthPrime();
+  else if (s == "strong")
+    isStrong();
+  else if (s == "factorial")
+    fact();
+}
+vector<string> files;
 void Initialise() {
 
   mp["even"] = "Programs/even.txt";
   mp["odd"] = "Programs/odd.txt";
   mp["even_odd"] = "Programs/Even_Odd.txt";
+  mp["palindrome"] = "Programs/palindrome.txt";
+  mp["reverse"] = "Programs/reverse.txt";
+  mp["nthprime"] = "Programs/nthprime.txt";
+  mp["strong"] = "Programs/isstrong.txt";
+  mp["armstrong"] = "Programs/armstrong.txt";
+  mp["prime"] = "Programs/prime.txt";
+  mp["happy"] = "Programs/happy.txt";
+  mp["factorial"] = "Program/factorial.txt";
+
+  files.push_back("even");
+  files.push_back("odd");
+  files.push_back("even_odd");
+  files.push_back("reverse");
+  files.push_back("palindrome");
+  files.push_back("strong");
+  files.push_back("happy");
+  files.push_back("prime");
+  files.push_back("armstrong");
+  files.push_back("nthprime");
+  files.push_back("factorial");
 }
 
 int Choice() {
@@ -33,8 +78,12 @@ void solve(string s) {
   stringstream ss(s);
   string token;
   bool check = false;
+
+  vector<string> tokens;
   while (ss >> token) {
     if (mp[token] != "") {
+
+      tokens.push_back(token);
       check = true;
       int choice = Choice();
       if (choice == 1) {
@@ -58,15 +107,33 @@ void solve(string s) {
 
         cout.tie(NULL);
       } else if (choice == 2) {
-        cout << "Enter Numbe to check value\n";
-        int value;
-        // check = true;
-        cin >> value;
-        if (value & 1)
-          cout << "Entered Number is: Odd\n";
-        else
-          cout << "Entered Number is: Even\n";
+        check = true;
+        callFun(token);
       }
+    }
+  }
+  if (tokens.size() > 1) {
+    cout << "Is above Provide Information Right\n1:True and 2 False:";
+    int op;
+    cin >> op;
+    if (op == 1)
+      return;
+    else if (op == 2) {
+      set<string> st;
+      for (int i = 0; i < tokens.size(); i++) {
+        for (int j = 0; j < files.size(); j++) {
+          if (files[j].find(tokens[i]) != string::npos)
+            st.insert(files[j]);
+        }
+      }
+      cout << "Choose Option from Below \n";
+      int count = 1;
+      for (auto &i : st) {
+        cout << count++ << ": " << i << endl;
+      }
+      string New;
+      cin >> New;
+      solve(New);
     }
   }
   // if (!check) {
@@ -80,15 +147,17 @@ void start() {
   string s;
   cout << "Press __ to Stop The Program" << endl;
   cout << endl;
-  cout << "Enter Your Query: ";
-  getline(cin, s);
 
-  while (s != "__") {
-    solve(s);
-    cout << endl;
+  do {
+
     cout << "Enter Your Query: ";
+
     getline(cin, s);
-  }
+    cout << endl;
+    solve(s);
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+  } while (s != "__");
 }
 void print() {
   cout << "****** ****** ****** *          * ******  ****** " << endl;
